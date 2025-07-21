f9599a360903e59fb13e9f03b4e22ca29fae1811
[Halimatou Waghe] <[waghehalimatou@gmail.com]>
2025-07-21 23:29:00 +0100

    Updating script.js file to include level 100 requirements

--- script.js
+++ script.js
@@ -6,7 +6,37 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  rootElem.innerHTML = "";
+  //rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+
+  const episodeCards = episodeList.map(createEpisodeCard);
+
+  rootElem.append(...episodeCards);
+}
+
+function createEpisodeCard(episode) {
+  const episodeCard = document
+    .getElementById("episode-card")
+    .content.cloneNode(true);
+
+  let seasonStr = episode.season.toString().padStart(2, "0");
+  let episodeStr = episode.number.toString().padStart(2, "0");
+  let episodeCode = "S" + seasonStr + "E" + episodeStr;
+
+  const title = episodeCard.querySelector("h1");
+  const titleLink = document.createElement("a");
+  titleLink.href = episode.url;
+  titleLink.textContent = episode.name;
+  titleLink.target = "_blank";
+  titleLink.rel = "noopener noreferrer";
+  title.replaceWith(titleLink);
+
+  episodeCard.querySelector("h2").textContent = episodeCode;
+  episodeCard.querySelector("img").src = episode.image?.medium;
+  episodeCard.querySelector("img").alt = `Image from ${episode.name}`;
+  episodeCard.querySelector("p").innerHTML = episode.summary;
+
+  return episodeCard;
 }
 
 window.onload = setup;
